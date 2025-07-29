a1d7d485bc11644b5890db45b928ae2fde358997
Jeff Brownlee <jeff.b.brownlee@gmail.com>
2025-07-28 19:13:51 -0500

    Fixed proxy file name

--- pages/api/clerk/[...proxy].ts
+++ pages/api/clerk/[...proxy].ts
@@ -1,22 +1,3 @@
-// api/clerk/[...proxy].ts
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import { createClerkProxy } from '@clerk/nextjs/server';
 
-export const config = {
-  api: {
-    bodyParser: false,
-    externalResolver: true,
-  },
-};
-
-const target = 'https://frontend-api.clerk.services';
-
-const proxy = createProxyMiddleware({
-  target,
-  changeOrigin: true,
-  pathRewrite: (path, req) => path.replace(/^\/api\/clerk/, ''),
-  logLevel: 'warn',
-});
-
-export default function handler(req: any, res: any) {
-  return proxy(req, res, () => {});
-}
+export default createClerkProxy();
